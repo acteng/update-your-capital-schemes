8d0b25e2f005391fa4f9622f0564d7d391ffd583
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-14 16:34:15 +0100

    GH-1: Introduce app factory

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+def create_app() -> Flask:
+    app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    @app.route("/")
+    def hello_world():
+        return "<p>Hello, World!</p>"
+
+    return app
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
+from flask import Flask
 from flask.testing import FlaskClient
 import pytest
-from schemes import app
+from schemes import create_app
 
 
 @pytest.fixture
-def client() -> FlaskClient:
+def app() -> Flask:
+    return create_app()
+
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
     return app.test_client()

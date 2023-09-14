c5c2a13dea51cad37f22404d908882b80a2786cb
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-14 17:35:10 +0100

    GH-1: Rename route

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -5,7 +5,7 @@ def create_app() -> Flask:
     app = Flask(__name__)
 
     @app.route("/")
-    def hello_world():
+    def index():
         return render_template("index.html")
 
     return app
--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -5,6 +5,6 @@ from playwright.sync_api import Page, expect
 
 @pytest.mark.usefixtures("live_server")
 def test_index(page: Page) -> None:
-    page.goto(url_for("hello_world", _external=True))
+    page.goto(url_for("index", _external=True))
 
     expect(page.get_by_role("paragraph")).to_contain_text("Hello, World!")

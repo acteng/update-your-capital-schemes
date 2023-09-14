3ed6d231fcd3ce29f579cf04d4e5cc882fd39837
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-14 16:43:36 +0100

    GH-1: Add HTML boilerplate

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ from flask.testing import FlaskClient
 def test_index(client: FlaskClient) -> None:
     response = client.get("/")
 
-    assert response.text == "<p>Hello, World!</p>"
+    assert "<p>Hello, World!</p>" in response.text

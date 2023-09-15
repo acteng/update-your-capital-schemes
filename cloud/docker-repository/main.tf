9c7b9e651805200073962a6357e11885d2c1c7aa
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-15 15:11:19 +0100

    GH-1: Push Docker image to repository

--- cloud/docker-repository/main.tf
+++ cloud/docker-repository/main.tf
@@ -6,10 +6,11 @@ terraform {
 }
 
 provider "google" {
-  project = "dft-ate-schemes-prt"
+  project = local.project
 }
 
 locals {
+  project  = "dft-ate-schemes-prt"
   location = "europe-west1"
 }
 
@@ -24,4 +25,26 @@ resource "google_artifact_registry_repository" "main" {
   format = "DOCKER"
 
   depends_on = [google_project_service.artifact_registry]
+}
+
+resource "google_service_account" "github_action" {
+  account_id   = "github-action"
+  display_name = "Service account for use within GitHub actions"
+}
+
+resource "google_project_iam_member" "github_action_service_account_token_creator" {
+  project = local.project
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.github_action.email}"
+}
+
+resource "google_project_iam_member" "github_action_artifact_registry_writer" {
+  project = local.project
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_action.email}"
+}
+
+resource "google_service_account_key" "github_action" {
+  service_account_id = google_service_account.github_action.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
 }
---
+++ cloud/docker-repository/outputs.tf
@@ -0,0 +1,5 @@
+output "github_action_private_key" {
+  description = "Service account key for github action service account"
+  value       = google_service_account_key.github_action.private_key
+  sensitive   = true
+}

3a7d9362c8673a29fab1a55657ecaf9cd49b838d
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-15 14:39:35 +0100

    GH-1: Pull up project to provider

--- cloud/tf-backend/main.tf
+++ cloud/tf-backend/main.tf
@@ -1,12 +1,14 @@
+provider "google" {
+  project = "dft-ate-schemes-prt"
+}
+
 locals {
   location = "europe-west1"
-  project  = "dft-ate-schemes-prt"
 }
 
 resource "google_storage_bucket" "main" {
   name     = "dft-ate-schemes-prt-tf-backend"
   location = local.location
-  project  = local.project
 
   public_access_prevention = "enforced"
 

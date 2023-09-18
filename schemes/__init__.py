a2e453b7b0a831fb10fde431fa1367ef40c8f8f6
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-18 18:00:06 +0100

    GH-1: Use govuk-frontend-jinja for GOV.UK markup

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -1,8 +1,17 @@
+import os
 from flask import Flask, render_template
+from jinja2 import ChoiceLoader, FileSystemLoader, PackageLoader, PrefixLoader
 
 
 def create_app() -> Flask:
     app = Flask(__name__, static_url_path="/")
+
+    app.jinja_loader = ChoiceLoader(
+        [
+            FileSystemLoader(os.path.join(app.root_path, app.template_folder)),
+            PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")})
+        ]
+    )
 
     @app.route("/")
     def index() -> str:

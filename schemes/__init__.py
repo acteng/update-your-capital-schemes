c84310af737100323fb72697c82e88e744ff8fa7
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-18 17:42:20 +0100

    GH-1: Apply GOV.UK Frontend style
    Installed with:
    
    ```
    wget https://github.com/alphagov/govuk-frontend/releases/download/v4.7.0/release-v4.7.0.zip
    unzip -d schemes/static release-v4.7.0.zip
    rm release-v4.7.0.zip schemes/static/VERSION.txt
    ```

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 
 def create_app() -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path="/")
 
     @app.route("/")
     def index() -> str:

271e533d91e71d7607ee0cf3ceaa552251021f49
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-18 18:25:39 +0100

    GH-1: Fix tests for GOV.UK styling

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -4,4 +4,4 @@ from flask.testing import FlaskClient
 def test_index(client: FlaskClient) -> None:
     response = client.get("/")
 
-    assert "<h1>Schemes</h1>" in response.text
+    assert '<h1 class="govuk-heading-xl">Schemes</h1>' in response.text

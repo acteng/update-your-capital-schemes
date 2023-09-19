43e7de1224ec8e85252f3572242a5598de6764c3
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-19 14:57:23 +0100

    GH-1: Moved GOVUK frontend resources into static

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from jinja2 import BaseLoader, ChoiceLoader, FileSystemLoader, PackageLoader, PrefixLoader
 
 
 def create_app() -> Flask:
-    app = Flask(__name__, static_url_path="/")
+    app = Flask(__name__)
     app.jinja_loader = create_jinja_loader(app)  # type: ignore
+
+    @app.context_processor
+    def govuk_frontend_config() -> dict[str, str]:
+        return {"assetPath": url_for("static", filename="/assets")}
 
     @app.route("/")
     def index() -> str:

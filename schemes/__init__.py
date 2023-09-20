7805badf743ad3d381ba0ff5a05a81cf0467e2ea
Alex Coleman <alex.coleman@dft.gov.uk>
2023-09-20 11:10:20 +0100

    GH-1: Add logic for Basic HTTP auth if an environment variable is set

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask, render_template, url_for
+import functools
+from typing import Callable, TypeVar, ParamSpec, Union
+
+from flask import Flask, render_template, url_for, Response, request
 from jinja2 import BaseLoader, ChoiceLoader, FileSystemLoader, PackageLoader, PrefixLoader
 
 
@@ -10,6 +13,12 @@ def create_app() -> Flask:
     @app.context_processor
     def govuk_frontend_config() -> dict[str, str]:
         return {"assetPath": url_for("static", filename="govuk-frontend/assets")}
+
+    if os.getenv("AUTH_USER"):
+        @app.before_request
+        @basic_auth
+        def before_request() -> None:
+            pass
 
     @app.route("/")
     def index() -> str:
@@ -22,3 +31,23 @@ def create_jinja_loader(app: Flask) -> BaseLoader:
     default_loader = FileSystemLoader(os.path.join(app.root_path, str(app.template_folder)))
     govuk_loader = PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")})
     return ChoiceLoader([default_loader, govuk_loader])
+
+def check_auth(username: Union[str, None], password: Union[str, None]) -> bool:
+    return os.getenv("AUTH_USER") == username and os.getenv("AUTH_PASSWORD") == password
+
+T = TypeVar('T')
+P = ParamSpec('P')
+
+def basic_auth(f: Callable[P, T]) -> Callable[P, Union[T, Response]]:
+    @functools.wraps(f)
+    def decorated_function(*args: P.args, **kwargs: P.kwargs) -> Union[T, Response]:
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return Response(
+                response="Unauthorized",
+                status=401,
+                headers={"WWW-Authenticate": "Basic realm='Login Required'"}
+            )
+        return f(*args, **kwargs)
+
+    return decorated_function

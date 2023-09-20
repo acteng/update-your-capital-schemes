0bd8cd3eb6649f928348a99d3e24d5ed3149db8f
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-20 08:13:45 +0100

    GH-1: Use absolute URLs in e2e tests

--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -1,10 +1,17 @@
 import pytest
-from flask import url_for
+from flask import Flask
 from playwright.sync_api import Page, expect
 
 
 @pytest.mark.usefixtures("live_server")
-def test_index(page: Page) -> None:
-    page.goto(url_for("index", _external=True))
+def test_index(app: Flask, page: Page) -> None:
+    page.goto(f"{_get_base_url(app)}/")
 
     expect(page.get_by_role("heading")).to_contain_text("Schemes")
+
+
+def _get_base_url(app: Flask) -> str:
+    scheme = app.config["PREFERRED_URL_SCHEME"]
+    server_name = app.config["SERVER_NAME"]
+    root = app.config["APPLICATION_ROOT"]
+    return f"{scheme}://{server_name}{root}"

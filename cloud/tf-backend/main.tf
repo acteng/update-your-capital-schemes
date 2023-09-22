8f956009d1e7a2a467f76cf15825f3e8629f2172
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-22 18:08:15 +0100

    GH-1: Inline project into resources

--- cloud/tf-backend/main.tf
+++ cloud/tf-backend/main.tf
@@ -1,13 +1,11 @@
-provider "google" {
-  project = "dft-ate-schemes"
-}
-
 locals {
+  project  = "dft-ate-schemes"
   location = "europe-west1"
 }
 
 resource "google_storage_bucket" "main" {
   name     = "dft-ate-schemes-tf-backend"
+  project  = local.project
   location = local.location
 
   public_access_prevention = "enforced"

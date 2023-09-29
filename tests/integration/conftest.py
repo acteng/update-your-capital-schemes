f8a1a3d8fb0d05c1ea1ffab5e5b35baa05e7e53b
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-09-29 18:13:39 +0100

    GH-2: Improve configuring basic auth in integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,3 +1,5 @@
+from typing import Any, Mapping
+
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
@@ -5,9 +7,14 @@ from flask.testing import FlaskClient
 from schemes import create_app
 
 
+@pytest.fixture(name="config")
+def config_fixture() -> Mapping[str, Any]:
+    return {"TESTING": True}
+
+
 @pytest.fixture(name="app")
-def app_fixture() -> Flask:
-    return create_app({"TESTING": True})
+def app_fixture(config: Mapping[str, Any]) -> Flask:
+    return create_app(config)
 
 
 @pytest.fixture(name="client")
--- tests/integration/test_basic_auth.py
+++ tests/integration/test_basic_auth.py
@@ -1,40 +1,34 @@
+from typing import Any, Mapping
+
+import pytest
 from flask.testing import FlaskClient
 
-from schemes import create_app
+
+class TestUnauthenticated:
+    def test_access_when_no_basic_auth(self, client: FlaskClient) -> None:
+        response = client.get("/")
+
+        assert response.status_code == 200
 
 
-def test_access_when_no_basic_auth(client: FlaskClient) -> None:
-    response = client.get("/")
+class TestAuthenticated:
+    @pytest.fixture(name="config")
+    def config_fixture(self, config: Mapping[str, Any]) -> Mapping[str, Any]:
+        return config | {"BASIC_AUTH_USERNAME": "alice", "BASIC_AUTH_PASSWORD": "letmein"}
 
-    assert response.status_code == 200
+    def test_challenge_when_basic_auth(self, client: FlaskClient) -> None:
+        response = client.get("/")
 
+        assert response.status_code == 401 and response.headers["WWW-Authenticate"] == "Basic realm='Schemes'"
 
-def test_challenge_when_basic_auth() -> None:
-    client = _create_test_client("alice", "letmein")
+    def test_access_when_basic_auth(self, client: FlaskClient) -> None:
+        # echo -n 'alice:letmein' | base64
+        response = client.get("/", headers={"Authorization": "Basic YWxpY2U6bGV0bWVpbg=="})
 
-    response = client.get("/")
+        assert response.status_code == 200
 
-    assert response.status_code == 401 and response.headers["WWW-Authenticate"] == "Basic realm='Schemes'"
+    def test_cannot_access_when_incorrect_basic_auth(self, client: FlaskClient) -> None:
+        # echo -n 'bob:opensesame' | base64
+        response = client.get("/", headers={"Authorization": "Basic Ym9iOm9wZW5zZXNhbWU="})
 
-
-def test_access_when_basic_auth() -> None:
-    client = _create_test_client("alice", "letmein")
-
-    # echo -n 'alice:letmein' | base64
-    response = client.get("/", headers={"Authorization": "Basic YWxpY2U6bGV0bWVpbg=="})
-
-    assert response.status_code == 200
-
-
-def test_cannot_access_when_incorrect_basic_auth() -> None:
-    client = _create_test_client("alice", "letmein")
-
-    # echo -n 'bob:opensesame' | base64
-    response = client.get("/", headers={"Authorization": "Basic Ym9iOm9wZW5zZXNhbWU="})
-
-    assert response.status_code == 401 and response.text == "Unauthorized"
-
-
-def _create_test_client(username: str, password: str) -> FlaskClient:
-    app = create_app({"BASIC_AUTH_USERNAME": username, "BASIC_AUTH_PASSWORD": password})
-    return app.test_client()
+        assert response.status_code == 401 and response.text == "Unauthorized"

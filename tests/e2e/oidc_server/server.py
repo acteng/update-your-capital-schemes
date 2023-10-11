1afd64f2ff865389a43770e6321211212e642c4d
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-10-11 15:45:34 +0100

    Extract default timeout

--- tests/e2e/oidc_server/server.py
+++ tests/e2e/oidc_server/server.py
@@ -6,17 +6,19 @@ from tests.e2e.oidc_server.users import StubUser
 
 
 class OidcServer(LiveServer):  # type: ignore
+    DEFAULT_TIMEOUT = 10
+
     def add_user(self, user: StubUser) -> None:
-        response = requests.post(f"{self._get_url()}/users", json=user.__dict__, timeout=10)
+        response = requests.post(f"{self._get_url()}/users", json=user.__dict__, timeout=self.DEFAULT_TIMEOUT)
         assert response.status_code == 201
 
     def clear_users(self) -> None:
-        response = requests.delete(f"{self._get_url()}/users", timeout=10)
+        response = requests.delete(f"{self._get_url()}/users", timeout=self.DEFAULT_TIMEOUT)
         assert response.status_code == 204
 
     def _get_url(self) -> str:
         return f"http://{self.host}:{self.port}"
 
     def add_client(self, client: StubClient) -> None:
-        response = requests.post(f"{self._get_url()}/clients", json=client.__dict__, timeout=10)
+        response = requests.post(f"{self._get_url()}/clients", json=client.__dict__, timeout=self.DEFAULT_TIMEOUT)
         assert response.status_code == 201

96852ea6e7b79cc669f3656ec43869e667243203
Alex Coleman <alex.coleman@dft.gov.uk>
2023-10-20 16:29:19 +0100

    GH-4: Refactor basic auth decorator

--- schemes/basic_auth.py
+++ schemes/basic_auth.py
@@ -10,16 +10,22 @@ P = ParamSpec("P")
 def basic_auth(func: Callable[P, T]) -> Callable[P, T | Response]:
     @functools.wraps(func)
     def wrapped_view(*args: P.args, **kwargs: P.kwargs) -> T | Response:
-        username = current_app.config.get("BASIC_AUTH_USERNAME")
-        password = current_app.config.get("BASIC_AUTH_PASSWORD")
-
-        if username:
-            auth = request.authorization
-            if not (auth and auth.type == "basic" and auth.username == username and auth.password == password):
-                return Response(
-                    status=401, headers={"WWW-Authenticate": "Basic realm='Schemes'"}, response="Unauthorized"
-                )
-
-        return func(*args, **kwargs)
+        return func(*args, **kwargs) if _authorized() else _challenge()
 
     return wrapped_view
+
+
+def _authorized() -> bool:
+    username = current_app.config.get("BASIC_AUTH_USERNAME")
+    password = current_app.config.get("BASIC_AUTH_PASSWORD")
+
+    if username:
+        auth = request.authorization
+        if not (auth and auth.type == "basic" and auth.username == username and auth.password == password):
+            return False
+
+    return True
+
+
+def _challenge() -> Response:
+    return Response(status=401, headers={"WWW-Authenticate": "Basic realm='Schemes'"}, response="Unauthorized")

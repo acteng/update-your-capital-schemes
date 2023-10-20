5d23916f53bd89017acd924df4cd5968e859c4ab
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-10-20 17:53:03 +0100

    GH-4: Make basic auth credential check more flexible

--- schemes/basic_auth.py
+++ schemes/basic_auth.py
@@ -2,36 +2,37 @@ import functools
 from typing import Callable, ParamSpec, TypeVar
 
 from flask import Response, current_app, request
+from werkzeug.datastructures import Authorization
 
 T = TypeVar("T")
 P = ParamSpec("P")
+Authorized = Callable[[Authorization | None], bool]
 
 
 def ui() -> Callable[[Callable[P, T]], Callable[P, T | Response]]:
-    return basic_auth("BASIC_AUTH_USERNAME", "BASIC_AUTH_PASSWORD")
+    def authorized(auth: Authorization | None) -> bool:
+        username = current_app.config.get("BASIC_AUTH_USERNAME")
+        return not username or _authorized(auth, username, current_app.config["BASIC_AUTH_PASSWORD"])
+
+    return basic_auth(authorized)
 
 
-def basic_auth(username_config: str, password_config: str) -> Callable[[Callable[P, T]], Callable[P, T | Response]]:
+def basic_auth(authorized: Authorized) -> Callable[[Callable[P, T]], Callable[P, T | Response]]:
     def decorator(func: Callable[P, T]) -> Callable[P, T | Response]:
         @functools.wraps(func)
         def decorated_function(*args: P.args, **kwargs: P.kwargs) -> T | Response:
-            return func(*args, **kwargs) if _authorized(username_config, password_config) else _challenge()
+            return func(*args, **kwargs) if authorized(request.authorization) else _challenge()
 
         return decorated_function
 
     return decorator
 
 
-def _authorized(username_config: str, password_config: str) -> bool:
-    username = current_app.config.get(username_config)
-    password = current_app.config.get(password_config)
+def _authorized(auth: Authorization | None, username: str, password: str) -> bool:
+    if not (auth and auth.type == "basic"):
+        return False
 
-    if username:
-        auth = request.authorization
-        if not (auth and auth.type == "basic" and auth.username == username and auth.password == password):
-            return False
-
-    return True
+    return auth.username == username and auth.password == password
 
 
 def _challenge() -> Response:

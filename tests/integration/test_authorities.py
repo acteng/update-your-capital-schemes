2f2754539016c17135ff67424e71f118e5d3bb6e
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-11-13 17:10:59 +0000

    Assert domain attributes directly instead of using tuples

--- tests/integration/test_authorities.py
+++ tests/integration/test_authorities.py
@@ -44,10 +44,11 @@ class TestApiEnabled:
         )
 
         assert response.status_code == 201
-        assert [_authority_to_tuple(authority) for authority in authorities.get_all()] == [
-            _authority_to_tuple(Authority(id_=1, name="Liverpool City Region Combined Authority")),
-            _authority_to_tuple(Authority(id_=2, name="West Yorkshire Combined Authority")),
-        ]
+        authority1: Authority
+        authority2: Authority
+        (authority1, authority2) = authorities.get_all()
+        assert authority1.id == 1 and authority1.name == "Liverpool City Region Combined Authority"
+        assert authority2.id == 2 and authority2.name == "West Yorkshire Combined Authority"
 
     def test_cannot_add_authorities_when_no_credentials(
         self, authorities: AuthorityRepository, client: FlaskClient
@@ -77,10 +78,11 @@ class TestApiEnabled:
         )
 
         assert response.status_code == 201
-        assert [_user_to_tuple(user) for user in users.get_all()] == [
-            _user_to_tuple(User("boardman@example.com", authority_id=1)),
-            _user_to_tuple(User("obree@example.com", authority_id=1)),
-        ]
+        user1: User
+        user2: User
+        (user1, user2) = users.get_all()
+        assert user1.email == "boardman@example.com" and user2.authority_id == 1
+        assert user2.email == "obree@example.com" and user2.authority_id == 1
 
     def test_cannot_add_users_when_no_credentials(self, users: UserRepository, client: FlaskClient) -> None:
         response = client.post("/authorities/1/users", json=[{"email": "boardman@example.com"}])
@@ -107,13 +109,17 @@ class TestApiEnabled:
         )
 
         assert response.status_code == 201
-        wirral_package = Scheme(id_=1, name="Wirral Package", authority_id=1)
-        wirral_package.type = SchemeType.CONSTRUCTION
-        wirral_package.funding_programme = FundingProgramme.ATF4
-        assert [_scheme_to_tuple(scheme) for scheme in schemes.get_all()] == [
-            _scheme_to_tuple(wirral_package),
-            _scheme_to_tuple(Scheme(id_=2, name="School Streets", authority_id=1)),
-        ]
+        scheme1: Scheme
+        scheme2: Scheme
+        (scheme1, scheme2) = schemes.get_all()
+        assert (
+            scheme1.id == 1
+            and scheme1.name == "Wirral Package"
+            and scheme1.authority_id == 1
+            and scheme1.type == SchemeType.CONSTRUCTION
+            and scheme1.funding_programme == FundingProgramme.ATF4
+        )
+        assert scheme2.id == 2 and scheme2.name == "School Streets" and scheme2.authority_id == 1
 
     def test_clear_authorities(self, authorities: AuthorityRepository, client: FlaskClient) -> None:
         authorities.add(Authority(id_=1, name="Liverpool City Region Combined Authority"))
@@ -152,15 +158,3 @@ class TestApiDisabled:
 
         assert response.status_code == 401
         assert [authority.id for authority in authorities.get_all()] == [1]
-
-
-def _authority_to_tuple(authority: Authority | None) -> tuple[int, str] | None:
-    return (authority.id, authority.name) if authority else None
-
-
-def _user_to_tuple(user: User | None) -> tuple[str, int] | None:
-    return (user.email, user.authority_id) if user else None
-
-
-def _scheme_to_tuple(scheme: Scheme | None) -> tuple[int, str, int, SchemeType | None, FundingProgramme | None] | None:
-    return (scheme.id, scheme.name, scheme.authority_id, scheme.type, scheme.funding_programme) if scheme else None

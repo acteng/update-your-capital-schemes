106296ce22a2e5ca5c988ec6859f3717fe290b6b
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-11-29 09:15:01 +0000

    Move schemes domain into package

---
+++ schemes/domain/schemes/__init__.py
@@ -0,0 +1,11 @@
+from schemes.domain.schemes.schemes import DataSource as DataSource
+from schemes.domain.schemes.schemes import DateRange as DateRange
+from schemes.domain.schemes.schemes import FinancialRevision as FinancialRevision
+from schemes.domain.schemes.schemes import FinancialType as FinancialType
+from schemes.domain.schemes.schemes import FundingProgramme as FundingProgramme
+from schemes.domain.schemes.schemes import Milestone as Milestone
+from schemes.domain.schemes.schemes import MilestoneRevision as MilestoneRevision
+from schemes.domain.schemes.schemes import ObservationType as ObservationType
+from schemes.domain.schemes.schemes import Scheme as Scheme
+from schemes.domain.schemes.schemes import SchemeRepository as SchemeRepository
+from schemes.domain.schemes.schemes import SchemeType as SchemeType

f89d17c767ed059edf0fcf9b13788d41f447bf65
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-12-07 10:09:36 +0000

    Move schemes milestones view classes into module

--- schemes/views/schemes/schemes.py
+++ schemes/views/schemes/schemes.py
@@ -14,8 +14,6 @@ from schemes.domain.authorities import Authority, AuthorityRepository
 from schemes.domain.schemes import (
     DateRange,
     FundingProgramme,
-    Milestone,
-    MilestoneRevision,
     ObservationType,
     OutputMeasure,
     OutputRevision,
@@ -29,6 +27,11 @@ from schemes.domain.users import UserRepository
 from schemes.views.auth.api_key import api_key_auth
 from schemes.views.auth.bearer import bearer_auth
 from schemes.views.schemes.funding import FinancialRevisionRepr, SchemeFundingContext
+from schemes.views.schemes.milestones import (
+    MilestoneContext,
+    MilestoneRevisionRepr,
+    SchemeMilestonesContext,
+)
 from schemes.views.schemes.observations import ObservationTypeRepr
 
 bp = Blueprint("schemes", __name__)
@@ -160,68 +163,6 @@ class FundingProgrammeContext:
     @classmethod
     def from_domain(cls, funding_programme: FundingProgramme | None) -> FundingProgrammeContext:
         return cls(name=cls._NAMES[funding_programme] if funding_programme else None)
-
-
-@dataclass(frozen=True)
-class MilestoneContext:
-    name: str | None
-    _NAMES = {
-        Milestone.PUBLIC_CONSULTATION_COMPLETED: "Public consultation completed",
-        Milestone.FEASIBILITY_DESIGN_COMPLETED: "Feasibility design completed",
-        Milestone.PRELIMINARY_DESIGN_COMPLETED: "Preliminary design completed",
-        Milestone.OUTLINE_DESIGN_COMPLETED: "Outline design completed",
-        Milestone.DETAILED_DESIGN_COMPLETED: "Detailed design completed",
-        Milestone.CONSTRUCTION_STARTED: "Construction started",
-        Milestone.CONSTRUCTION_COMPLETED: "Construction completed",
-        Milestone.INSPECTION: "Inspection",
-        Milestone.NOT_PROGRESSED: "Not progressed",
-        Milestone.SUPERSEDED: "Superseded",
-        Milestone.REMOVED: "Removed",
-    }
-
-    @classmethod
-    def from_domain(cls, milestone: Milestone | None) -> MilestoneContext:
-        return cls(name=cls._NAMES[milestone] if milestone else None)
-
-
-@dataclass(frozen=True)
-class SchemeMilestonesContext:
-    milestones: list[SchemeMilestoneRowContext]
-
-    @classmethod
-    def from_domain(cls, milestone_revisions: list[MilestoneRevision]) -> SchemeMilestonesContext:
-        def get_status_date(milestone: Milestone, observation_type: ObservationType) -> date | None:
-            revisions = (
-                revision.status_date
-                for revision in milestone_revisions
-                if revision.milestone == milestone and revision.observation_type == observation_type
-            )
-            return next(revisions, None)
-
-        return cls(
-            milestones=[
-                SchemeMilestoneRowContext(
-                    milestone=MilestoneContext.from_domain(milestone),
-                    planned=get_status_date(milestone, ObservationType.PLANNED),
-                    actual=get_status_date(milestone, ObservationType.ACTUAL),
-                )
-                for milestone in [
-                    Milestone.PUBLIC_CONSULTATION_COMPLETED,
-                    Milestone.FEASIBILITY_DESIGN_COMPLETED,
-                    Milestone.PRELIMINARY_DESIGN_COMPLETED,
-                    Milestone.DETAILED_DESIGN_COMPLETED,
-                    Milestone.CONSTRUCTION_STARTED,
-                    Milestone.CONSTRUCTION_COMPLETED,
-                ]
-            ]
-        )
-
-
-@dataclass(frozen=True)
-class SchemeMilestoneRowContext:
-    milestone: MilestoneContext
-    planned: date | None
-    actual: date | None
 
 
 @dataclass(frozen=True)
@@ -406,57 +347,6 @@ class FundingProgrammeRepr(Enum):
             FundingProgrammeRepr.MRN: FundingProgramme.MRN,
             FundingProgrammeRepr.LUF: FundingProgramme.LUF,
             FundingProgrammeRepr.CRSTS: FundingProgramme.CRSTS,
-        }
-        return members[self]
-
-
-@dataclass(frozen=True)
-class MilestoneRevisionRepr:
-    effective_date_from: str
-    effective_date_to: str | None
-    milestone: MilestoneRepr
-    observation_type: ObservationTypeRepr
-    status_date: str
-
-    def to_domain(self) -> MilestoneRevision:
-        return MilestoneRevision(
-            effective=DateRange(
-                date_from=date.fromisoformat(self.effective_date_from),
-                date_to=date.fromisoformat(self.effective_date_to) if self.effective_date_to else None,
-            ),
-            milestone=self.milestone.to_domain(),
-            observation_type=self.observation_type.to_domain(),
-            status_date=date.fromisoformat(self.status_date),
-        )
-
-
-@unique
-class MilestoneRepr(Enum):
-    PUBLIC_CONSULTATION_COMPLETED = "public consultation completed"
-    FEASIBILITY_DESIGN_COMPLETED = "feasibility design completed"
-    PRELIMINARY_DESIGN_COMPLETED = "preliminary design completed"
-    OUTLINE_DESIGN_COMPLETED = "outline design completed"
-    DETAILED_DESIGN_COMPLETED = "detailed design completed"
-    CONSTRUCTION_STARTED = "construction started"
-    CONSTRUCTION_COMPLETED = "construction completed"
-    INSPECTION = "inspection"
-    NOT_PROGRESSED = "not progressed"
-    SUPERSEDED = "superseded"
-    REMOVED = "removed"
-
-    def to_domain(self) -> Milestone:
-        members = {
-            MilestoneRepr.PUBLIC_CONSULTATION_COMPLETED: Milestone.PUBLIC_CONSULTATION_COMPLETED,
-            MilestoneRepr.FEASIBILITY_DESIGN_COMPLETED: Milestone.FEASIBILITY_DESIGN_COMPLETED,
-            MilestoneRepr.PRELIMINARY_DESIGN_COMPLETED: Milestone.PRELIMINARY_DESIGN_COMPLETED,
-            MilestoneRepr.OUTLINE_DESIGN_COMPLETED: Milestone.OUTLINE_DESIGN_COMPLETED,
-            MilestoneRepr.DETAILED_DESIGN_COMPLETED: Milestone.DETAILED_DESIGN_COMPLETED,
-            MilestoneRepr.CONSTRUCTION_STARTED: Milestone.CONSTRUCTION_STARTED,
-            MilestoneRepr.CONSTRUCTION_COMPLETED: Milestone.CONSTRUCTION_COMPLETED,
-            MilestoneRepr.INSPECTION: Milestone.INSPECTION,
-            MilestoneRepr.NOT_PROGRESSED: Milestone.NOT_PROGRESSED,
-            MilestoneRepr.SUPERSEDED: Milestone.SUPERSEDED,
-            MilestoneRepr.REMOVED: Milestone.REMOVED,
         }
         return members[self]
 
---
+++ tests/views/schemes/test_milestones.py
@@ -0,0 +1,169 @@
+from datetime import date
+
+import pytest
+
+from schemes.domain.schemes import (
+    DateRange,
+    Milestone,
+    MilestoneRevision,
+    ObservationType,
+)
+from schemes.views.schemes.milestones import (
+    MilestoneContext,
+    MilestoneRepr,
+    MilestoneRevisionRepr,
+    SchemeMilestoneRowContext,
+    SchemeMilestonesContext,
+)
+from schemes.views.schemes.observations import ObservationTypeRepr
+
+
+class TestSchemeMilestonesContext:
+    def test_from_domain_sets_milestones(self) -> None:
+        context = SchemeMilestonesContext.from_domain([])
+
+        assert [row.milestone for row in context.milestones] == [
+            MilestoneContext(name="Public consultation completed"),
+            MilestoneContext(name="Feasibility design completed"),
+            MilestoneContext(name="Preliminary design completed"),
+            MilestoneContext(name="Detailed design completed"),
+            MilestoneContext(name="Construction started"),
+            MilestoneContext(name="Construction completed"),
+        ]
+
+    @pytest.mark.parametrize(
+        "milestone, expected_milestone_name",
+        [
+            (Milestone.PUBLIC_CONSULTATION_COMPLETED, "Public consultation completed"),
+            (Milestone.FEASIBILITY_DESIGN_COMPLETED, "Feasibility design completed"),
+            (Milestone.PRELIMINARY_DESIGN_COMPLETED, "Preliminary design completed"),
+            (Milestone.DETAILED_DESIGN_COMPLETED, "Detailed design completed"),
+            (Milestone.CONSTRUCTION_STARTED, "Construction started"),
+            (Milestone.CONSTRUCTION_COMPLETED, "Construction completed"),
+        ],
+    )
+    def test_from_domain_sets_milestone_dates(self, milestone: Milestone, expected_milestone_name: str) -> None:
+        milestone_revisions = [
+            MilestoneRevision(
+                effective=DateRange(date(2020, 1, 1), None),
+                milestone=milestone,
+                observation_type=ObservationType.PLANNED,
+                status_date=date(2020, 2, 1),
+            ),
+            MilestoneRevision(
+                effective=DateRange(date(2020, 1, 1), None),
+                milestone=milestone,
+                observation_type=ObservationType.ACTUAL,
+                status_date=date(2020, 3, 1),
+            ),
+        ]
+
+        context = SchemeMilestonesContext.from_domain(milestone_revisions)
+
+        assert (
+            SchemeMilestoneRowContext(
+                milestone=MilestoneContext(name=expected_milestone_name),
+                planned=date(2020, 2, 1),
+                actual=date(2020, 3, 1),
+            )
+            in context.milestones
+        )
+
+    @pytest.mark.parametrize(
+        "expected_milestone_name",
+        [
+            "Public consultation completed",
+            "Feasibility design completed",
+            "Preliminary design completed",
+            "Detailed design completed",
+            "Construction started",
+            "Construction completed",
+        ],
+    )
+    def test_from_domain_sets_milestone_dates_when_no_revisions(self, expected_milestone_name: str) -> None:
+        context = SchemeMilestonesContext.from_domain([])
+
+        assert (
+            SchemeMilestoneRowContext(
+                milestone=MilestoneContext(name=expected_milestone_name), planned=None, actual=None
+            )
+            in context.milestones
+        )
+
+
+class TestMilestoneContext:
+    @pytest.mark.parametrize(
+        "milestone, expected_name",
+        [
+            (Milestone.PUBLIC_CONSULTATION_COMPLETED, "Public consultation completed"),
+            (Milestone.FEASIBILITY_DESIGN_COMPLETED, "Feasibility design completed"),
+            (Milestone.PRELIMINARY_DESIGN_COMPLETED, "Preliminary design completed"),
+            (Milestone.OUTLINE_DESIGN_COMPLETED, "Outline design completed"),
+            (Milestone.DETAILED_DESIGN_COMPLETED, "Detailed design completed"),
+            (Milestone.CONSTRUCTION_STARTED, "Construction started"),
+            (Milestone.CONSTRUCTION_COMPLETED, "Construction completed"),
+            (Milestone.INSPECTION, "Inspection"),
+            (Milestone.NOT_PROGRESSED, "Not progressed"),
+            (Milestone.SUPERSEDED, "Superseded"),
+            (Milestone.REMOVED, "Removed"),
+            (None, None),
+        ],
+    )
+    def test_from_domain(self, milestone: Milestone | None, expected_name: str | None) -> None:
+        context = MilestoneContext.from_domain(milestone)
+
+        assert context == MilestoneContext(name=expected_name)
+
+
+class TestMilestoneRevisionRepr:
+    def test_to_domain(self) -> None:
+        milestone_revision_repr = MilestoneRevisionRepr(
+            effective_date_from="2020-01-01",
+            effective_date_to="2020-01-31",
+            milestone=MilestoneRepr.DETAILED_DESIGN_COMPLETED,
+            observation_type=ObservationTypeRepr.ACTUAL,
+            status_date="2020-01-01",
+        )
+
+        milestone_revision = milestone_revision_repr.to_domain()
+
+        assert milestone_revision == MilestoneRevision(
+            effective=DateRange(date(2020, 1, 1), date(2020, 1, 31)),
+            milestone=Milestone.DETAILED_DESIGN_COMPLETED,
+            observation_type=ObservationType.ACTUAL,
+            status_date=date(2020, 1, 1),
+        )
+
+    def test_to_domain_when_no_effective_date_to(self) -> None:
+        milestone_revision_repr = MilestoneRevisionRepr(
+            effective_date_from="2020-01-01",
+            effective_date_to=None,
+            milestone=MilestoneRepr.DETAILED_DESIGN_COMPLETED,
+            observation_type=ObservationTypeRepr.ACTUAL,
+            status_date="2020-01-01",
+        )
+
+        milestone_revision = milestone_revision_repr.to_domain()
+
+        assert milestone_revision.effective.date_to is None
+
+
+class TestMilestoneRepr:
+    @pytest.mark.parametrize(
+        "milestone, expected_milestone",
+        [
+            ("public consultation completed", Milestone.PUBLIC_CONSULTATION_COMPLETED),
+            ("feasibility design completed", Milestone.FEASIBILITY_DESIGN_COMPLETED),
+            ("preliminary design completed", Milestone.PRELIMINARY_DESIGN_COMPLETED),
+            ("outline design completed", Milestone.OUTLINE_DESIGN_COMPLETED),
+            ("detailed design completed", Milestone.DETAILED_DESIGN_COMPLETED),
+            ("construction started", Milestone.CONSTRUCTION_STARTED),
+            ("construction completed", Milestone.CONSTRUCTION_COMPLETED),
+            ("inspection", Milestone.INSPECTION),
+            ("not progressed", Milestone.NOT_PROGRESSED),
+            ("superseded", Milestone.SUPERSEDED),
+            ("removed", Milestone.REMOVED),
+        ],
+    )
+    def test_to_domain(self, milestone: str, expected_milestone: Milestone) -> None:
+        assert MilestoneRepr(milestone).to_domain() == expected_milestone

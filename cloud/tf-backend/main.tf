0537fa478ee29d0bcd3b93ef5c5ae8bcd4714851
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2023-12-11 12:14:29 +0000

    Enable uniform bucket level access for tf-backend

--- cloud/tf-backend/main.tf
+++ cloud/tf-backend/main.tf
@@ -8,9 +8,16 @@ resource "google_storage_bucket" "main" {
   project  = local.project
   location = local.location
 
+  uniform_bucket_level_access = true
   public_access_prevention = "enforced"
 
   versioning {
     enabled = true
   }
+}
+
+resource "google_storage_bucket_iam_member" "main_storage_object_user" {
+  bucket = google_storage_bucket.main.name
+  member = "projectEditor:${local.project}"
+  role   = "roles/storage.objectUser"
 }

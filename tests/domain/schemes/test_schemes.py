300efd7558d0be2b4fbd533283c1a6bd28eddb7d
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-04-05 10:49:48 +0100

    GH-93: Decompose scheme is-updateable test

--- tests/domain/schemes/test_schemes.py
+++ tests/domain/schemes/test_schemes.py
@@ -56,30 +56,45 @@ class TestScheme:
         assert isinstance(scheme.reviews, SchemeReviews)
 
     @pytest.mark.parametrize(
-        "bid_status, milestone, expected_updateable",
+        "bid_status, expected_updateable",
         [
-            pytest.param(BidStatus.SUBMITTED, Milestone.PUBLIC_CONSULTATION_COMPLETED, False, id="not funded"),
-            pytest.param(BidStatus.FUNDED, Milestone.PUBLIC_CONSULTATION_COMPLETED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.FEASIBILITY_DESIGN_STARTED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.FEASIBILITY_DESIGN_COMPLETED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.PRELIMINARY_DESIGN_COMPLETED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.OUTLINE_DESIGN_COMPLETED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.DETAILED_DESIGN_COMPLETED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.CONSTRUCTION_STARTED, True, id="updateable"),
-            pytest.param(BidStatus.FUNDED, Milestone.CONSTRUCTION_COMPLETED, False, id="complete"),
-            pytest.param(BidStatus.FUNDED, Milestone.INSPECTION, False, id="complete"),
-            pytest.param(BidStatus.FUNDED, Milestone.NOT_PROGRESSED, False, id="inactive"),
-            pytest.param(BidStatus.FUNDED, Milestone.SUPERSEDED, False, id="inactive"),
-            pytest.param(BidStatus.FUNDED, Milestone.REMOVED, False, id="inactive"),
-            pytest.param(BidStatus.NOT_FUNDED, Milestone.PUBLIC_CONSULTATION_COMPLETED, False, id="not funded"),
-            pytest.param(BidStatus.SPLIT, Milestone.PUBLIC_CONSULTATION_COMPLETED, False, id="not funded"),
-            pytest.param(BidStatus.DELETED, Milestone.PUBLIC_CONSULTATION_COMPLETED, False, id="not funded"),
+            (BidStatus.SUBMITTED, False),
+            (BidStatus.FUNDED, True),
+            (BidStatus.NOT_FUNDED, False),
+            (BidStatus.SPLIT, False),
+            (BidStatus.DELETED, False),
         ],
     )
-    def test_is_updateable(self, bid_status: BidStatus, milestone: Milestone, expected_updateable: bool) -> None:
+    def test_is_updateable_when_funded(self, bid_status: BidStatus, expected_updateable: bool) -> None:
         scheme = Scheme(id_=1, name="Wirral Package", authority_id=2)
         scheme.funding.update_bid_status(
             BidStatusRevision(id_=3, effective=DateRange(datetime(2000, 1, 2), None), status=bid_status)
+        )
+        scheme.milestones.update_milestone(
+            MilestoneRevision(
+                id_=4,
+                effective=DateRange(datetime(2000, 1, 2), None),
+                milestone=Milestone.PUBLIC_CONSULTATION_COMPLETED,
+                observation_type=ObservationType.ACTUAL,
+                status_date=date(2000, 1, 2),
+                source=DataSource.ATF4_BID,
+            )
+        )
+
+        assert scheme.is_updateable == expected_updateable
+
+    @pytest.mark.parametrize(
+        "milestone, expected_updateable",
+        [
+            pytest.param(Milestone.PUBLIC_CONSULTATION_COMPLETED, True, id="active and incomplete"),
+            pytest.param(Milestone.CONSTRUCTION_COMPLETED, False, id="complete"),
+            pytest.param(Milestone.NOT_PROGRESSED, False, id="inactive"),
+        ],
+    )
+    def test_is_updateable_when_active_and_incomplete(self, milestone: Milestone, expected_updateable: bool) -> None:
+        scheme = Scheme(id_=1, name="Wirral Package", authority_id=2)
+        scheme.funding.update_bid_status(
+            BidStatusRevision(id_=3, effective=DateRange(datetime(2000, 1, 2), None), status=BidStatus.FUNDED)
         )
         scheme.milestones.update_milestone(
             MilestoneRevision(

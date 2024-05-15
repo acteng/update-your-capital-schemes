15add59b44fc16f453bc565c1fba3748ab5c7935
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-05-15 15:14:49 +0100

    GH-96: Confirm authorization endpoint error behaviour

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -1,7 +1,8 @@
-from typing import Any, Mapping
+from typing import Any, Generator, Mapping
 from unittest.mock import Mock
 
 import pytest
+from authlib.integrations.base_client import OAuthError
 from authlib.integrations.flask_client import OAuth
 from authlib.oidc.core import UserInfo
 from flask import current_app, session
@@ -16,8 +17,13 @@ class TestAuth:
         return dict(config) | {"GOVUK_END_SESSION_ENDPOINT": "https://example.com/logout"}
 
     @pytest.fixture(name="oauth")
-    def oauth_fixture(self) -> OAuth:
-        return current_app.extensions["authlib.integrations.flask_client"]
+    def oauth_fixture(self) -> Generator[OAuth, None, None]:
+        oauth = current_app.extensions["authlib.integrations.flask_client"]
+        previous_authorize_access_token = oauth.govuk.authorize_access_token
+        previous_userinfo = oauth.govuk.userinfo
+        yield oauth
+        oauth.govuk.authorize_access_token = previous_authorize_access_token
+        oauth.govuk.userinfo = previous_userinfo
 
     def test_callback_logs_in(self, oauth: OAuth, users: UserRepository, client: FlaskClient) -> None:
         users.add(User("boardman@example.com", authority_id=1))
@@ -37,6 +43,12 @@ class TestAuth:
         response = client.get("/auth")
 
         assert response.status_code == 302 and response.location == "/schemes"
+
+    def test_callback_when_authentication_error_raises_error(self, users: UserRepository, client: FlaskClient) -> None:
+        users.add(User("boardman@example.com", authority_id=1))
+
+        with pytest.raises(OAuthError, match="invalid_request: Unsupported response"):
+            client.get("/auth", query_string={"error": "invalid_request", "error_description": "Unsupported response"})
 
     def test_callback_when_unauthorized_returns_forbidden(
         self, oauth: OAuth, users: UserRepository, client: FlaskClient

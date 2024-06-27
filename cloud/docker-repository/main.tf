86b82d30135756c1dffb15e6f2602db5126c1af5
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2024-06-27 17:04:04 +0100

    GH-118: Delete untagged Docker images after a week

--- cloud/docker-repository/main.tf
+++ cloud/docker-repository/main.tf
@@ -6,8 +6,9 @@ terraform {
 }
 
 locals {
-  project  = "dft-schemes-common"
-  location = "europe-west1"
+  project        = "dft-schemes-common"
+  location       = "europe-west1"
+  day_in_seconds = 24 * 60 * 60
 }
 
 resource "google_project_service" "iam_credentials" {
@@ -25,12 +26,37 @@ resource "google_project_service" "container_scanning" {
   service = "containerscanning.googleapis.com"
 }
 
+resource "google_project_iam_audit_config" "artifact_registry_data_write" {
+  project = local.project
+  service = "artifactregistry.googleapis.com"
+
+  audit_log_config {
+    log_type = "DATA_WRITE"
+  }
+}
+
 resource "google_artifact_registry_repository" "main" {
   project       = local.project
   repository_id = "docker"
   location      = local.location
+  format        = "DOCKER"
 
-  format = "DOCKER"
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state = "UNTAGGED"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-recent-untagged"
+    action = "KEEP"
+    condition {
+      tag_state  = "UNTAGGED"
+      newer_than = "${7 * local.day_in_seconds}s"
+    }
+  }
 
   depends_on = [google_project_service.artifact_registry]
 }

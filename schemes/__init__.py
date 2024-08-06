0103d040cdc055b5ba418ce2f7e7894c194a7ceb
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-08-06 18:03:45 +0100

    Log environment on startup

--- schemes/__init__.py
+++ schemes/__init__.py
@@ -71,6 +71,7 @@ def create_app(test_config: Mapping[str, Any] | None = None) -> Flask:
     app.config.from_mapping(test_config)
 
     _configure_logger(app)
+    app.logger.info("Using environment '%s'", env)
 
     inject.configure(bindings(app), bind_in_runtime=False)
 

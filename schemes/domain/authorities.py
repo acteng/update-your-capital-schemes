d57a3aeee59c2ee37395ec3926825a75ad8e74fa
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-08-30 09:18:47 +0100

    Make authority domain attributes read-only

--- schemes/domain/authorities.py
+++ schemes/domain/authorities.py
@@ -1,7 +1,15 @@
 class Authority:
     def __init__(self, id_: int, name: str):
-        self.id = id_
-        self.name = name
+        self._id = id_
+        self._name = name
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
 
 
 class AuthorityRepository:

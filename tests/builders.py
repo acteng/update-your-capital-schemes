972a5fedc335d67fb51f3b4e6747630acb8ca3bb
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-09-03 09:00:28 +0100

    Ensure bid status and revisions are mutually exclusive

--- tests/builders.py
+++ tests/builders.py
@@ -20,13 +20,16 @@ def build_scheme(
     type_: SchemeType | None = None,
     funding_programme: FundingProgramme | None = None,
     overview_revisions: list[OverviewRevision] | None = None,
-    bid_status: BidStatus = BidStatus.FUNDED,
+    bid_status: BidStatus | None = None,
     bid_status_revisions: list[BidStatusRevision] | None = None,
 ) -> Scheme:
     if any((name is not None, authority_id is not None, type_ is not None, funding_programme is not None)) == (
         overview_revisions is not None
     ):
         assert False, "Either overview fields or revisions must be specified"
+
+    if bid_status is not None and bid_status_revisions is not None:
+        assert False, "Either bid status or revisions must be specified"
 
     if overview_revisions is not None:
         overview_revisions = overview_revisions
@@ -47,7 +50,9 @@ def build_scheme(
     bid_status_revisions = (
         bid_status_revisions
         if bid_status_revisions is not None
-        else [BidStatusRevision(id_=None, effective=DateRange(datetime.min, None), status=bid_status)]
+        else [
+            BidStatusRevision(id_=None, effective=DateRange(datetime.min, None), status=bid_status or BidStatus.FUNDED)
+        ]
     )
 
     scheme = Scheme(id_, reference)

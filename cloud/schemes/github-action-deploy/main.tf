52a88ce88ca56377575a5a1dcfcfbe39b1192ca8
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2024-11-27 17:25:51 +0000

    GH-171: Explicitly allow deploy service account to read repository

--- cloud/schemes/github-action-deploy/main.tf
+++ cloud/schemes/github-action-deploy/main.tf
@@ -22,6 +22,12 @@ resource "google_service_account_iam_member" "service_account_user" {
   member             = "serviceAccount:${google_service_account.main.email}"
 }
 
+resource "google_project_iam_member" "artifact_registry_reader" {
+  project = "dft-schemes-common"
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.main.email}"
+}
+
 resource "google_service_account_key" "main" {
   service_account_id = google_service_account.main.name
   public_key_type    = "TYPE_X509_PEM_FILE"

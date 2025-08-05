826e1cd0472d0d3420d429b4474ca9222caabef1
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-08-05 10:58:44 +0100

    Correct resource protector signature

--- tests/e2e/oidc_server/resources.py
+++ tests/e2e/oidc_server/resources.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Any, Callable
 
 from authlib.integrations.flask_oauth2 import ResourceProtector
 from flask import Response
@@ -6,7 +6,7 @@ from flask import Response
 
 class TypedResourceProtector(ResourceProtector):  # type: ignore
     def __call__[**P, T](
-        self, scopes: str | None = None, optional: bool = False
+        self, scopes: str | None = None, optional: bool = False, **kwargs: Any
     ) -> Callable[[Callable[P, T]], Callable[P, T | Response]]:
-        decorator: Callable[[Callable[P, T]], Callable[P, T | Response]] = super().__call__(scopes, optional)
+        decorator: Callable[[Callable[P, T]], Callable[P, T | Response]] = super().__call__(scopes, optional, **kwargs)
         return decorator

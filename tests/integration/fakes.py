c3a221c52c51c1881d19b173b47ab2748c2c5c75
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-05 10:52:22 +0100

    GH-206: Make ITs async
    ITs typically invoke repositories which are now async, so make these tests async
    too.
    
    Also migrate our fake repositories used in ITs to async as well.

--- tests/integration/fakes.py
+++ tests/integration/fakes.py
@@ -9,14 +9,14 @@ class MemoryAuthorityRepository(AuthorityRepository):
     def __init__(self) -> None:
         self._authorities: dict[str, Authority] = {}
 
-    def add(self, *authorities: Authority) -> None:
+    async def add(self, *authorities: Authority) -> None:
         for authority in authorities:
             self._authorities[authority.abbreviation] = authority
 
-    def clear(self) -> None:
+    async def clear(self) -> None:
         self._authorities.clear()
 
-    def get(self, abbreviation: str) -> Authority | None:
+    async def get(self, abbreviation: str) -> Authority | None:
         return self._authorities.get(abbreviation)
 
 
@@ -38,17 +38,17 @@ class MemorySchemeRepository(SchemeRepository):
     def __init__(self) -> None:
         self._schemes: dict[str, Scheme] = {}
 
-    def add(self, *schemes: Scheme) -> None:
+    async def add(self, *schemes: Scheme) -> None:
         for scheme in schemes:
             self._schemes[scheme.reference] = scheme
 
-    def clear(self) -> None:
+    async def clear(self) -> None:
         self._schemes.clear()
 
-    def get(self, reference: str) -> Scheme | None:
+    async def get(self, reference: str) -> Scheme | None:
         return deepcopy(self._schemes.get(reference))
 
-    def get_by_authority(self, authority_abbreviation: str) -> list[Scheme]:
+    async def get_by_authority(self, authority_abbreviation: str) -> list[Scheme]:
         return sorted(
             [
                 deepcopy(scheme)
@@ -58,5 +58,5 @@ class MemorySchemeRepository(SchemeRepository):
             key=lambda scheme: scheme.id,
         )
 
-    def update(self, scheme: Scheme) -> None:
+    async def update(self, scheme: Scheme) -> None:
         self._schemes[scheme.reference] = scheme

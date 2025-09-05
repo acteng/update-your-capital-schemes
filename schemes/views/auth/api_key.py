0fdea197755856a9e0946c0aed8a4d19e1de9a87
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-05 10:51:45 +0100

    GH-206: Make views async
    In order to use the new async repositories we need to make our Flask views
    async. Although Flask is predominately sync, it does support adapting async
    views [1], so we install that extra package.
    
    Async views then require async decorators. Provide async variants of our API key
    and bearer decorators.
    
    [1] https://flask.palletsprojects.com/en/stable/async-await/

--- schemes/views/auth/api_key.py
+++ schemes/views/auth/api_key.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from typing import Callable
+from typing import Awaitable, Callable
 
 from flask import abort, current_app, request
 
@@ -7,10 +7,23 @@ from flask import abort, current_app, request
 def api_key_auth[**P, T](func: Callable[P, T]) -> Callable[P, T]:
     @wraps(func)
     def decorated_function(*args: P.args, **kwargs: P.kwargs) -> T:
-        api_key = current_app.config.get("API_KEY")
-        auth = request.authorization
-        if not (auth and auth.type == "api-key" and auth.token == api_key):
-            abort(401)
+        _check_api_key()
         return func(*args, **kwargs)
 
     return decorated_function
+
+
+def async_api_key_auth[**P, T](func: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
+    @wraps(func)
+    async def decorated_function(*args: P.args, **kwargs: P.kwargs) -> T:
+        _check_api_key()
+        return await func(*args, **kwargs)
+
+    return decorated_function
+
+
+def _check_api_key() -> None:
+    api_key = current_app.config.get("API_KEY")
+    auth = request.authorization
+    if not (auth and auth.type == "api-key" and auth.token == api_key):
+        abort(401)

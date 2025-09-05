d30a20cff34ec69a749dd0a2ebec773a361a4ae7
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-05 16:43:08 +0100

    Simplify API key decorator now it raises exceptions
    Moved from an error response to raising an exception in:
    49c6866194ce96032db649ddde1303ceb1b031a6

--- schemes/views/auth/api_key.py
+++ schemes/views/auth/api_key.py
@@ -1,12 +1,12 @@
 from functools import wraps
 from typing import Callable
 
-from flask import Response, abort, current_app, request
+from flask import abort, current_app, request
 
 
-def api_key_auth[**P, T](func: Callable[P, T]) -> Callable[P, T | Response]:
+def api_key_auth[**P, T](func: Callable[P, T]) -> Callable[P, T]:
     @wraps(func)
-    def decorated_function(*args: P.args, **kwargs: P.kwargs) -> T | Response:
+    def decorated_function(*args: P.args, **kwargs: P.kwargs) -> T:
         api_key = current_app.config.get("API_KEY")
         auth = request.authorization
         if not (auth and auth.type == "api-key" and auth.token == api_key):

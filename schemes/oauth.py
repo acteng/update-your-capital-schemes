2b1dde24bdebf8cf2149d6093930a18bc0dec184
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-05 10:47:25 +0100

    GH-206: Use an async OAuth client for ATE API
    Authlib doesn't provide an async OAuth client for Flask [1], so build a custom
    one from the available mixins that uses HTTPX instead of Requests.
    
    Moving away from the standard Flask OAuth client means that the token is no
    longer cached in the application context within a request [2]. This doesn't
    impact our token caching [3] since we also cache the token across requests by
    storing it within `OAuthExtension`.
    
    This also requires reworking the workaround to support audience [4] to align
    with the async implementation.
    
    Enable async test support in Pytest and migrate from Responses to RESPX for
    mocking HTTP.
    
    [1] https://github.com/authlib/authlib/issues/818
    [2] https://github.com/authlib/authlib/issues/818#issuecomment-3257950062
    [3] https://github.com/acteng/update-your-capital-schemes/issues/198
    [4] https://github.com/authlib/authlib/issues/783

--- schemes/oauth.py
+++ schemes/oauth.py
@@ -1,44 +1,44 @@
-from authlib.integrations.flask_client import FlaskOAuth2App, OAuth
-from authlib.integrations.requests_client import OAuth2Session
+from authlib.integrations.base_client import BaseApp, InvalidTokenError
+from authlib.integrations.base_client.async_app import AsyncOAuth2Mixin
+from authlib.integrations.base_client.async_openid import AsyncOpenIDMixin
+from authlib.integrations.flask_client import OAuth
+from authlib.integrations.httpx_client import AsyncOAuth2Client
 from authlib.oauth2.rfc6749 import OAuth2Token
 from authlib.oauth2.rfc7523 import PrivateKeyJWT
-from flask import Flask
+from flask import Flask, Request
 
 
-class _AccessTokenParamsOAuth2Session(OAuth2Session):  # type: ignore
+class _AccessTokenParamsAsyncOAuth2Client(AsyncOAuth2Client):  # type: ignore
     """
     An OAuth2 session that supports extra parameters to fetch access token.
 
     See: https://github.com/authlib/authlib/issues/783
     """
 
-    def ensure_active_token(self, token: OAuth2Token = None) -> bool:
+    async def ensure_active_token(self, token: OAuth2Token = None) -> None:
         access_token_params = self.metadata.get("access_token_params") or {}
 
-        # Copy of OAuth2Client.ensure_active_token:
-        if token is None:
-            token = self.token
-        if not token.is_expired(leeway=self.leeway):
-            return True
-        refresh_token = token.get("refresh_token")
-        url = self.metadata.get("token_endpoint")
-        if refresh_token and url:
-            self.refresh_token(url, refresh_token=refresh_token)
-            return True
-        elif self.metadata.get("grant_type") == "client_credentials":
-            access_token = token["access_token"]
-            # Include extra parameters to fetch access token
-            new_token = self.fetch_token(url, grant_type="client_credentials", **access_token_params)
-            if self.update_token:
-                self.update_token(new_token, access_token=access_token)
-            return True
-
-        return False
+        # Copy of AsyncOAuth2Client.ensure_active_token:
+        async with self._token_refresh_lock:
+            if self.token.is_expired(leeway=self.leeway):
+                refresh_token = token.get("refresh_token")
+                url = self.metadata.get("token_endpoint")
+                if refresh_token and url:
+                    await self.refresh_token(url, refresh_token=refresh_token)
+                elif self.metadata.get("grant_type") == "client_credentials":
+                    access_token = token["access_token"]
+                    # Include extra parameters to fetch access token
+                    new_token = await self.fetch_token(url, grant_type="client_credentials", **access_token_params)
+                    if self.update_token:
+                        await self.update_token(new_token, access_token=access_token)
+                else:
+                    raise InvalidTokenError()
 
 
-class _AccessTokenParamsFlaskOAuth2App(FlaskOAuth2App):  # type: ignore
+# Workaround: https://github.com/authlib/authlib/issues/818
+class _AccessTokenParamsAsyncFlaskOAuth2App(AsyncOAuth2Mixin, AsyncOpenIDMixin, BaseApp):  # type: ignore
     # Workaround: https://github.com/authlib/authlib/issues/783
-    client_cls = _AccessTokenParamsOAuth2Session
+    client_cls = _AccessTokenParamsAsyncOAuth2Client
 
 
 class OAuthExtension(OAuth):  # type: ignore
@@ -61,7 +61,7 @@ class OAuthExtension(OAuth):  # type: ignore
             access_token_params = {"audience": app.config["ATE_AUDIENCE"]}
             self.register(
                 name="ate",
-                client_cls=_AccessTokenParamsFlaskOAuth2App,
+                client_cls=_AccessTokenParamsAsyncFlaskOAuth2App,
                 fetch_token=self._fetch_ate_token,
                 update_token=self._update_ate_token,
                 client_id=app.config["ATE_CLIENT_ID"],
@@ -78,12 +78,12 @@ class OAuthExtension(OAuth):  # type: ignore
                 },
             )
 
-    def _fetch_ate_token(self) -> OAuth2Token:
+    async def _fetch_ate_token(self, request: Request) -> OAuth2Token:
         if not self._ate_token:
-            self._ate_token = self.ate.fetch_access_token()
+            self._ate_token = await self.ate.fetch_access_token()
         return self._ate_token
 
-    def _update_ate_token(
+    async def _update_ate_token(
         self, token: OAuth2Token, refresh_token: str | None = None, access_token: str | None = None
     ) -> None:
         self._ate_token = token

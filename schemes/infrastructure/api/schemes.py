1f90a4609b5a82510e794a01adadd80cd5deaae6
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-09-09 18:38:45 +0100

    WIP: Request schemes in parallel

--- schemes/infrastructure/api/schemes.py
+++ schemes/infrastructure/api/schemes.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 from enum import Enum
 from typing import Annotated, Any
@@ -36,10 +37,12 @@ class ApiSchemeRepository(SchemeRepository):
         response.raise_for_status()
 
         collection_model = CollectionModel[AnyUrl].model_validate(response.json())
-        return [
-            await self._get_by_url(str(capital_scheme_url), funding_programmes)
-            for capital_scheme_url in collection_model.items
-        ]
+        return await asyncio.gather(
+            *[
+                self._get_by_url(str(capital_scheme_url), funding_programmes)
+                for capital_scheme_url in collection_model.items
+            ]
+        )
 
     async def _get_funding_programmes(self) -> dict[str, FundingProgramme]:
         response = await self._remote_app.get(

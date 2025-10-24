34194e7be612508a617de403f54c5b5c2ea614e3
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-10-24 17:51:43 +0100

    Correct resource protector type parameters

--- tests/e2e/oidc_server/resources.py
+++ tests/e2e/oidc_server/resources.py
@@ -1,12 +1,11 @@
 from typing import Any, Callable
 
 from authlib.integrations.flask_oauth2 import ResourceProtector
-from flask import Response
 
 
 class TypedResourceProtector(ResourceProtector):  # type: ignore
     def __call__[**P, T](
         self, scopes: str | None = None, optional: bool = False, **kwargs: Any
-    ) -> Callable[[Callable[P, T]], Callable[P, T | Response]]:
-        decorator: Callable[[Callable[P, T]], Callable[P, T | Response]] = super().__call__(scopes, optional, **kwargs)
+    ) -> Callable[[Callable[P, T]], Callable[P, T]]:
+        decorator: Callable[[Callable[P, T]], Callable[P, T]] = super().__call__(scopes, optional, **kwargs)
         return decorator

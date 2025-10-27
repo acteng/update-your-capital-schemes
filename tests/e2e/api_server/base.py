15c9b4d055dc4aaf17ca510e77b94a83ce9a7bbd
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2025-10-27 17:03:03 +0000

    GH-189: Correct E2E API server JSON responses to use camel case

--- tests/e2e/api_server/base.py
+++ tests/e2e/api_server/base.py
@@ -9,4 +9,4 @@ class BaseModel(pydantic_BaseModel):
     model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
     def to_json(self) -> dict[str, Any]:
-        return self.model_dump(mode="json")
+        return self.model_dump(mode="json", by_alias=True)

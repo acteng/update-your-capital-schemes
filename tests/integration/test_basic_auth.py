060319022d322c4d925041b7c733304242c63ba6
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-03-18 16:00:49 +0000

    Improve basic auth IT test names

--- tests/integration/test_basic_auth.py
+++ tests/integration/test_basic_auth.py
@@ -4,30 +4,30 @@ import pytest
 from flask.testing import FlaskClient
 
 
-class TestBasicAuthWhenUnauthenticated:
-    def test_access_when_no_basic_auth(self, client: FlaskClient) -> None:
+class TestBasicAuthWhenPublic:
+    def test_can_access(self, client: FlaskClient) -> None:
         response = client.get("/")
 
         assert response.status_code == 200
 
 
-class TestBasicAuthWhenAuthenticated:
+class TestBasicAuthWhenProtected:
     @pytest.fixture(name="config", scope="class")
     def config_fixture(self, config: Mapping[str, Any]) -> Mapping[str, Any]:
         return dict(config) | {"BASIC_AUTH_USERNAME": "boardman", "BASIC_AUTH_PASSWORD": "letmein"}
 
-    def test_challenge_when_basic_auth(self, client: FlaskClient) -> None:
+    def test_cannot_access_without_credentials(self, client: FlaskClient) -> None:
         response = client.get("/")
 
         assert response.status_code == 401 and response.headers["WWW-Authenticate"] == "Basic realm='Schemes'"
 
-    def test_access_when_basic_auth(self, client: FlaskClient) -> None:
+    def test_can_access_with_correct_credentials(self, client: FlaskClient) -> None:
         # echo -n 'boardman:letmein' | base64
         response = client.get("/", headers={"Authorization": "Basic Ym9hcmRtYW46bGV0bWVpbg=="})
 
         assert response.status_code == 200
 
-    def test_cannot_access_when_incorrect_basic_auth(self, client: FlaskClient) -> None:
+    def test_cannot_access_when_incorrect_credentials(self, client: FlaskClient) -> None:
         # echo -n 'obree:opensesame' | base64
         response = client.get("/", headers={"Authorization": "Basic b2JyZWU6b3BlbnNlc2FtZQ=="})
 

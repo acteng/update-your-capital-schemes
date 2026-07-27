90cae3008cff3e103a6cf0b212e4e0fdf704620a
Alex Coleman <alex.coleman@activetravelengland.gov.uk>
2026-07-27 12:47:26 +0100

    Upgrade Cloud SQL Auth Proxy to 2.24.1

--- cloud/schemes/cloud-run/main.tf
+++ cloud/schemes/cloud-run/main.tf
@@ -108,7 +108,7 @@ resource "google_cloud_run_v2_service" "schemes" {
     }
 
     containers {
-      image = "gcr.io/cloud-sql-connectors/cloud-sql-proxy:2.21.0"
+      image = "gcr.io/cloud-sql-connectors/cloud-sql-proxy:2.24.1"
       args = [
         "--address=0.0.0.0",
         "--port=5432",
